73d90969c81278839d496f8f29e4a71555cdb9a7
Roman Kalitovskyy <roman.kalitovskyy@gmail.com>
2021-05-07 13:10:21 +0300

    Base routing in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import NotFound from './pages/NotFound/NotFound';
+import Login from './pages/Login/Login';
+import Signup from './pages/Signup/Signup';
+import JobList from './pages/JobList/JobList';
+import PinnedJobs from './pages/PinnedJobs/PinnedJobs';
+import SelectedJobs from './pages/SelectedJobs/SelectedJobs';
+
 function App() {
-    return <></>;
+    return (
+        <div>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path='/'>
+                        <Home />
+                    </Route>
+                    <Route exact path='/login'>
+                        <Login />
+                    </Route>
+                    <Route exact path='/signup'>
+                        <Signup />
+                    </Route>
+                    <Route exact path='/jobList'>
+                        <JobList />
+                    </Route>
+                    <Route exact path='/pinnedJobs'>
+                        <PinnedJobs />
+                    </Route>
+                    <Route exact path='/selectedJobs'>
+                        <SelectedJobs />
+                    </Route>
+                    <Route path='*'>
+                        <NotFound />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const NotFound = () => <div>404</div>;
+export default NotFound;
