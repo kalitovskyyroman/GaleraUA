cd952168961801baeeeb6605c01e7c77155cf2b1
Roman Kalitovskyy <roman.kalitovskyy@gmail.com>
2021-05-10 10:33:21 +0300

    add global styles

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createGlobalStyle } from 'styled-components';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const Global = createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+`;
+
 ReactDOM.render(
     <React.StrictMode>
+        <Global />
         <App />
     </React.StrictMode>,
     document.getElementById('root'),
