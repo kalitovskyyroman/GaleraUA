291b5473c7eef1288137428b39d99c55818a304d
Roman Kalitovskyy <roman.kalitovskyy@gmail.com>
2021-05-11 21:44:15 +0300

    update button styles

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -4,44 +4,34 @@ import PropTypes from 'prop-types';
 const btnSize = {
     small: {
         fontSize: '10px',
-        paddingLeft: '10px',
-        paddingRight: '10px',
         borderRadius: '5px',
-        padding: '3px',
+        padding: '3px 10px',
     },
     regular: {
         fontSize: '20px',
-        paddingLeft: '20px',
-        paddingRight: '20px',
         borderRadius: '5px',
-        padding: '5px',
+        padding: '5px 20px',
     },
     medium: {
         fontSize: '25px',
-        paddingLeft: '22px',
-        paddingRight: '22px',
         borderRadius: '5px',
-        padding: '5px',
+        padding: '5px 22px',
     },
     large: {
         fontSize: '30px',
-        paddingLeft: '25px',
-        paddingRight: '25px',
         borderRadius: '7px',
-        padding: '5px',
+        padding: '5px 25px',
     },
 };
 
 const StyledButton = styled.button`
-    font-size: ${props => btnSize[props.size].fontSize || btnSize.regular.fontSize};
+    font-size: ${({ size }) => btnSize[size].fontSize || btnSize.regular.fontSize};
     font-weight: bold;
-    padding: ${props => btnSize[props.size].padding || btnSize.regular.padding};
-    padding-left: ${props => btnSize[props.size].paddingLeft || btnSize.regular.paddingLeft};
-    padding-right: ${props => btnSize[props.size].paddingRight || btnSize.regular.paddingRight};
-    background-color: ${props => props.theme.btnColors[props.mode] || props.theme.btnColors.regular};
+    padding: ${({ size }) => btnSize[size].padding || btnSize.regular.padding};
+    background-color: ${({ mode, theme }) => theme.btnColors[mode] || theme.btnColors.regular};
     border: none;
-    border-radius: ${props => btnSize[props.size].borderRadius || btnSize.regular.borderRadius};
-    color: ${props => props.theme.btnColors.textColor};
+    border-radius: ${({ size }) => btnSize[size].borderRadius || btnSize.regular.borderRadius};
+    color: ${({ theme }) => theme.btnColors.textColor};
     &: hover {
         cursor: pointer;
         opacity: 0.7;
