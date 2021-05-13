96d84a9ecfed46920e507951a93d612d583b26d8
Roman Kalitovskyy <roman.kalitovskyy@gmail.com>
2021-05-13 11:35:21 +0300

    delete ddiv

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 import routes from './AsideConfig';
 
 const Aside = () => (
-    <div>
+    <>
         {routes.map(route => (
             <NavLink key={route.path} exact to={route.path}>
                 {route.text}
             </NavLink>
         ))}
-    </div>
+    </>
 );
 export default Aside;
