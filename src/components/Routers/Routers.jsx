8934f45b0dc2878a758d682d53a72e39e0cb3bf1
Roman Kalitovskyy <roman.kalitovskyy@gmail.com>
2021-05-15 13:14:23 +0300

    update 404 path

--- src/components/Routers/Routers.jsx
+++ src/components/Routers/Routers.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import NotFound from '../../pages/NotFound/NotFound';
 import getAvailableRouters, { getPublicRouters } from '../../utils/AvailableRouters';
+import { paths } from '../../config/routers';
 
 const Routers = () => {
     const user = JSON.parse(localStorage.getItem('User'));
@@ -15,7 +16,7 @@ const Routers = () => {
                 : getAvailableRouters(user.role).map(route => (
                       <Route key={route.path} path={route.path} component={route.component} exact={route.exact} />
                   ))}
-            <Route path='*' component={NotFound} />
+            <Route path={paths.notFound} component={NotFound} />
         </Switch>
     );
 };
