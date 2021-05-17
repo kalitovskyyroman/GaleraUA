1911fafe2df407d2d0297cf71a22f28922c8c996
Roman Kalitovskyy <roman.kalitovskyy@gmail.com>
2021-05-17 11:40:24 +0300

    add SocialLogos

--- src/components/Footer/SocialLogos/SocialLogo/SocialLogo.jsx
+++ src/components/Footer/SocialLogos/SocialLogo/SocialLogo.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import SocialLogoStyles from './style';
 
-const SocialLogo = ({ img, ref }) => (
-    <a href={ref}>
-        <SocialLogoStyles src={img} />
+const SocialLogo = ({ img, alt, href }) => (
+    <a href={href}>
+        <SocialLogoStyles src={img} alt={alt} />
     </a>
 );
 
-SocialLogo.propTypes = { img: PropTypes.string.isRequired, ref: PropTypes.string.isRequired };
+SocialLogo.propTypes = {
+    img: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired,
+    href: PropTypes.string.isRequired,
+};
 
 export default SocialLogo;
--- src/components/Footer/SocialLogos/SocialLogos.jsx
+++ src/components/Footer/SocialLogos/SocialLogos.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import SocialLogo from './SocialLogo/SocialLogo';
+import imgs from './config';
+import SocialLogosStyles from './style';
 
 const SocialLogos = () => (
-    <>
-        <SocialLogo />
-    </>
+    <SocialLogosStyles>
+        {imgs.map(img => (
+            <SocialLogo key={img.ref} img={img.logo} alt={img.alt} href={img.ref} />
+        ))}
+    </SocialLogosStyles>
 );
 
 export default SocialLogos;
