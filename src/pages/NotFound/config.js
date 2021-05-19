1338a435a0ebab77b3e4d475081f1d02afb16631
kalivovskyyroman <roman.kalitovskyy@gmail.com>
2021-05-19 16:17:13 +0300

    add codes to config

--- src/pages/NotFound/config.js
+++ src/pages/NotFound/config.js
@@ -1,8 +1,9 @@
 import leoLogo from '../../assets/images/logo/leo_logo.png';
+import codes from '../../config/codes';
 
 const config = {
     text: 'Not found',
-    code: 404,
+    code: codes.NotFound,
     logo: leoLogo,
 };
 
