99f5ff24e83cb661be693b1958ec587d605040d1
kalivovskyyroman <roman.kalitovskyy@gmail.com>
2021-05-28 09:58:51 +0300

    add user name text

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React, { useCallback, useContext } from 'react';
 import { useHistory } from 'react-router-dom';
-import StyledHeader from './styles';
+import StyledHeader, { StyledUserName } from './styles';
 import Title from './Title/Title';
 import Button from '../Button/Button';
 import SearchBar from '../SearchBar/SearchBar';
@@ -11,14 +11,10 @@ import paths from '../../config/paths';
 import { UserContext } from '../../Context/User/userContext';
 
 const Header = () => {
-    const { user, setIsAuthenticated } = useContext(UserContext);
+    const { user } = useContext(UserContext);
 
     const history = useHistory();
     const logInHandle = useCallback(() => history.push(paths.login), [history]);
-    const logOutHandle = useCallback(() => {
-        history.push(paths.home);
-        setIsAuthenticated(false);
-    }, [history]);
 
     return (
         <StyledHeader>
@@ -26,13 +22,8 @@ const Header = () => {
             <SearchBar />
             {user.isAuthenticated ? (
                 <>
-                    {/* <Button mode='primary' size='large' onClick={logOutHandle}>
-                        {user.data.email}
-                    </Button> */}
+                    <StyledUserName>{user.data.email}</StyledUserName>
                     <UserLogo />
-                    {/* <Button mode='primary' onClick={logOutHandle}>
-                        {text.logout}
-                    </Button> */}
                 </>
             ) : (
                 <Button mode='primary' size='large' onClick={logInHandle}>
--- src/components/Modals/ProfileModal/styles.js
+++ src/components/Modals/ProfileModal/styles.js
@@ -17,6 +17,6 @@ export const StyledModalContent = styled.div`
     right: 50px;
     padding: 20px;
     border-radius: 10px;
-    background-color: white;
+    background-color: ${({ theme }) => theme.colors.white};
     box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
 `;
